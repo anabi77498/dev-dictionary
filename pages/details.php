ff729ca7dcc0faa0970b0030a3eaffab31ce6735
Asad Nabi <asadnabi7@gmail.com>
2023-03-29 23:09:42 -0400

    details p3m1 complete

---
+++ pages/details.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <title>Developer Dictionary</title>
+
+</head>
+
+<header>
+  <img src="/public/images/placeholder.jpg" alt="placeholder img" height=50 width=50>
+  <h1>The Developer Dictionary</h1>
+  <cite>Item was sourced at <a href="https://www.slntechnologies.com/home/image-placeholder/">slntechnologies</a></cite>
+</header>
+
+<body>
+  <p>Welcome to the developer dictionary!</p>
+  <img src="/public/images/placeholder.jpg" alt="placeholder img" height=200 width=200>
+
+  <h1>JavaScript</h1>
+  <p>Random facts about JavaScript, Random facts about JavaScript, Random facts about JavaScript, Random facts about JavaScript, Random facts about JavaScript, Random facts about JavaScript, Random facts about JavaScript, Random facts about JavaScript, Random facts about JavaScript</p>
+
+</body>
+
+</html>
